272e4ddaffffd283c9226fcc40289db0d6b0c1c5
Aibar Arpish <aibar201329@gmail.com>
2024-01-26 21:53:44 +0600

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int M = GetNumber();
+int N = GetNumber();
+Print(M,N);
+
+int GetNumber(){
+    Console.WriteLine("Enter the number: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void Print(int M, int N){
+    if(M<=N){
+        Console.WriteLine(M);
+        Print(++M, N);
+    }
+}
