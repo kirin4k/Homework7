c5d4a3a9f775b7679e9f330f61de868ec49074fd
Aibar Arpish <aibar201329@gmail.com>
2024-01-26 22:07:15 +0600

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = {1,5,9,7,10};
+Reverse(array);
+
+void Reverse(int[] arr, int i = 0){;
+    if(i<arr.Length){
+        Reverse(arr, i+1);
+         Console.WriteLine(arr[i]);
+    }
+
+}
