0b41d7cddb62e89f78c8f20166534f35c7b80140
Aibar Arpish <aibar201329@gmail.com>
2024-01-26 21:54:37 +0600

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = GetNumber();
+int m = GetNumber();
+Console.WriteLine(Ackerman(n,m));
+
+int GetNumber(){
+    Console.WriteLine("Enter the postive number: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int Ackerman(int n, int m){
+    if(n==0){
+        return m+1;
+    }
+    else if (m==0){
+        return Ackerman(n-1,1);
+    }
+    else
+    return Ackerman(n-1, Ackerman(n,m-1));
+}
